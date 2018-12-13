4926948d3f60b6102899580ee9c674442c7ebcfe
mdifelice <mdifelice@live.com.ar>
2018-12-13 20:12:41 -0300

    First draft

--- ct-user-list.php
+++ ct-user-list.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Codeable Test User List
  * Description: Allows to print a list of users
- * Author:      MartÃ­n Di Felice
+ * Author:      Martín Di Felice
  * Author URI:  https://github.com/mdifelice
  * Text Domain: ct-user-list
  * Domain Path: /languages
@@ -15,20 +15,20 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-define( 'CT_USER_LIST_PAGE_LENGTH',     10 );
-define( 'CT_USER_LIST_GET_USERS_NONCE', 'ct-user-list-get-users-nonce' );
+define( 'CT_USER_LIST_PAGE_LENGTH', 10 );
+define( 'CT_USER_LIST_GET_USER_DATA_NONCE', 'ct-user-list-get-user-data-nonce' );
 
 /**
  * Returns a list of users.
  *
  * @param array/string $options {
- * 	Options to be considered when retrieving users from the database.
+ *  Options to be considered when retrieving users from the database.
  *
- * 	@type string $role     Roles should be retrieved. Default is any role.
- * 	@type string $order_by Field to be used to order users.
- * 	@type string $order    Type of order: ASC (ascending) or DESC (descending).
- * 	                       Default is ascending.
- * 	@type int    $page     Page to retrieve. Default is one.
+ *  @type string $role     Roles should be retrieved. Default is any role.
+ *  @type string $order_by Field to be used to order users.
+ *  @type string $order    Type of order: ASC (ascending) or DESC (descending).
+ *                         Default is ascending.
+ *  @type int    $page     Page to retrieve. Default is one.
  * }
  *
  * @return array User information {
@@ -44,48 +44,48 @@ function ct_user_list_get_user_data( $options = null ) {
 		'page'     => 1,
 	);
 
-	$options = wp_parse_args( $defaults, $options );
-
-	$roles = ct_user_list_get_roles();
+	$options    = wp_parse_args( $options, $defaults );
+	$roles      = ct_user_list_get_roles();
+	$fields     = ct_user_list_get_fields();
+	$query_args = array(
+		'number'      => CT_USER_LIST_PAGE_LENGTH,
+		'count_total' => true,
+	);
 
 	if ( isset( $roles[ $options['role'] ] ) ) {
-		$role = $options['role'];
-	} else {
-		$role = $defaults['role'];
+		$query_args['role'] = $options['role'];
 	}
-
-	$fields   = ct_user_list_get_fields();
-	$order_by = $defaults['order_by'];
 
 	if ( isset( $fields[ $options['order_by'] ] ) ) {
 		$field = $fields[ $options['order_by'] ];
 
-		if ( $field['sortable'] ) {
-			$order_by = $options['order_by'];
+		if ( $field['order_field'] ) {
+			$order_field = $field['order_field'];
+
+			if ( preg_match( '/^meta_key=(.+)$/', $order_field, $matches ) ) {
+				$order_by               = 'meta_value';
+				$query_args['meta_key'] = $matches[1];
+			} else {
+				$order_by = $order_field;
+			}
+
+			$query_args['orderby'] = $order_by;
 		}
 	}
 
 	if ( in_array( strtolower( $options['order'] ), array( 'asc', 'desc' ), true ) ) {
-		$order = $options['order'];
-	} else {
-		$order = $defaults['order'];
+		$query_args['order'] = $options['order'];
 	}
 
 	$page = absint( $options['page'] );
 
 	if ( ! $page ) {
-		$page = $defauts['page'];
+		$page = $defaults['page'];
 	}
 
-	$query = new WP_User_Query( array(
-		'role'        => $role,
-		'order_by'    => $order_by,
-		'order'       => $order,
-		'paged'       => $page,
-		'number'      => CT_USER_LIST_PAGE_LENGTH,
-		'count_total' => true,
-	) );
+	$query_args['paged'] = $page;
 
+	$query   = new WP_User_Query( $query_args );
 	$users   = array();
 	$results = $query->get_results();
 
@@ -106,10 +106,10 @@ function ct_user_list_get_user_data( $options = null ) {
 }
 
 /**
- * @todo
+ * Handles the AJAX call for retrieving user data.
  */
-function ct_user_list_ajax_get_users() {
-	check_ajax_referer( CT_USER_LIST_GET_USERS_NONCE );
+function ct_user_list_ajax_get_user_data() {
+	check_ajax_referer( CT_USER_LIST_GET_USER_DATA_NONCE );
 
 	$options             = array();
 	$whitelisted_options = array(
@@ -121,17 +121,23 @@ function ct_user_list_ajax_get_users() {
 
 	foreach ( $whitelisted_options as $whitelisted_option ) {
 		if ( isset( $_POST[ $whitelisted_option ] ) ) {
-			$options[ $whitelisted_option ] = sanitize_text_field( wp_unslash( $_POST[ $whitelisted_option ] ) );
+			$value = $_POST[ $whitelisted_option ];
+
+			if ( is_string( $value ) ) {
+				$options[ $whitelisted_option ] = sanitize_text_field( wp_unslash( $value ) );
+			}
 		}
 	}
 
-	$users = ct_user_list_get_users( $options );
+	$users = ct_user_list_get_user_data( $options );
 
 	wp_send_json( $users );
 }
 
 /**
- * @todo
+ * Returns a list of user roles and their descriptions.
+ *
+ * @return array User roles.
  */
 function ct_user_list_get_roles() {
 	$all_roles = WP_Roles()->roles;
@@ -144,21 +150,23 @@ function ct_user_list_get_roles() {
 }
 
 /**
- * @todo
+ * Returns the list of fields that will be displayed in the shortcode.
+ *
+ * @return array List of fields.
  */
 function ct_user_list_get_fields() {
 	return array(
 		'username'   => array(
-			'label'    => __( 'Username', 'ct-user-list' ),
-			'sortable' => true,
-			'callback' => function( $user ) {
+			'label'       => __( 'Username', 'ct-user-list' ),
+			'order_field' => 'username',
+			'callback'    => function( $user ) {
 				return $user->user_login;
-			}
+			},
 		),
 		'first_name' => array(
-			'label'    => __( 'First Name', 'ct-user-list' ),
-			'sortable' => true,
-			'callback' => function( $user ) {
+			'label'       => __( 'First Name', 'ct-user-list' ),
+			'order_field' => 'meta_key=first_name',
+			'callback'    => function( $user ) {
 				$user_data  = get_userdata( $user->ID );
 				$first_name = '';
 
@@ -167,7 +175,7 @@ function ct_user_list_get_fields() {
 				}
 
 				return $first_name;
-			}
+			},
 		),
 		'role'       => array(
 			'label'    => __( 'Role', 'ct-user-list' ),
@@ -187,73 +195,102 @@ function ct_user_list_get_fields() {
 				}
 
 				return $role;
-			}
+			},
 		),
 	);
 }
 
-add_action( 'wp_ajax_nopriv_ct_user_list_get_users', 'ct_user_list_ajax_get_users' );
-add_action( 'wp_ajax_priv_ct_user_list_get_users', 'ct_user_list_ajax_get_users' );
+add_action( 'wp_ajax_nopriv_ct_user_list_get_user_data', 'ct_user_list_ajax_get_user_data' );
+add_action( 'wp_ajax_ct_user_list_get_user_data', 'ct_user_list_ajax_get_user_data' );
 
-add_action( 'wp_enqueue_scripts', function() {
-	wp_enqueue_script(
-		'ct-user-list',
-		plugins_url( 'public/js/ct-user-list.js', __FILE__ ),
-		array( 'jquery' ),
-		'1',
-		true
-	);
-} );
+add_action(
+	'wp_enqueue_scripts',
+	function() {
+		wp_register_script(
+			'ct-user-list',
+			plugins_url( 'public/js/ct-user-list.js', __FILE__ ),
+			array( 'jquery' ),
+			'1',
+			true
+		);
 
-add_action( 'init', function() {
-	load_plugin_textdomain( 'ct-user-list', false, basename( dirname( __FILE__ ) ) . '/languages' ); 
-} );
-
-add_shortcode( 'ct-user-list', function() {
-	$html = '<div id="ct-user-list-container">';
-	$html .= '<form class="ct-user-list-search" action="' . esc_url( admin_url( 'admin-ajax.php' ) ) . '">';
-	$html .= '<select name="role">';
-	$html .= '<option value="">';
-	$html .= esc_html__( 'Select role...', 'ct-user-list' );
-	$html .= '</option>';
-
-	$roles = ct_user_list_get_roles();
-
-	foreach ( $roles as $role => $label ) {
-		$html .= '<option value="' . esc_attr( $role ) . '">';
-		$html .= esc_html( $label );
-		$html .= '</option>';
-	}
-
-	$html .= '</select>';
-	$html .= '<input type="hidden" name="page" />';
-	$html .= '<input type="hidden" name="order" />';
-	$html .= '<input type="hidden" name="order_by" />';
-	$html .= '<input type="hidden" name="action" value="ct_user_list_get_users" />';
-	$html .= '<input type="hidden" name="nonce" value="' . esc_attr( wp_create_nonce( CT_USER_LIST_GET_USERS_NONCE ) ) . '" />';
-	$html .= '</form>';
-	$html .= '<div class="ct-user-list">';
-	$html .= '</div>';
-	$html .= '</div>';
-
-	wp_enqueue_script( 'ct-user-list' );
-
-	$fields = ct_user_list_get_fields();
-
-	foreach ( $fields as $id => $field ) {
-		$headers[ $id ] = array(
-			'label'    => $field['label'],
-			'sortable' => $field['sortable'],
+		wp_register_style(
+			'ct-user-list',
+			plugins_url( 'public/css/ct-user-list.css', __FILE__ ),
+			array(),
+			'1'
 		);
 	}
+);
 
-	wp_localize_script( 'ct-user-list', 'ct_user_list_options', array(
-		'data'    => ct_user_list_get_user_data(),
-		'headers' => $headers,
-		'labels'  => array(
-			'notFound' => __( 'No users found', 'ct-user-list' ),
-		),
-	) );
+add_action(
+	'init',
+	function() {
+		if ( ! is_admin() ) {
+			load_textdomain( 'default', WP_LANG_DIR . '/admin-' . get_locale() . '.mo' );
+		}
 
-	return $html;
-} );
+		load_plugin_textdomain( 'ct-user-list', false, basename( dirname( __FILE__ ) ) . '/languages' );
+	}
+);
+
+add_shortcode(
+	'ct_user_list',
+	function() {
+		$html  = '<div id="ct-user-list-container">';
+		$html .= '<form class="ct-user-list-search" action="' . esc_url( admin_url( 'admin-ajax.php' ) ) . '">';
+		$html .= '<select name="role">';
+		$html .= '<option value="">';
+		$html .= esc_html__( 'Any role', 'ct-user-list' );
+		$html .= '</option>';
+
+		$roles = ct_user_list_get_roles();
+
+		foreach ( $roles as $role => $label ) {
+			$html .= '<option value="' . esc_attr( $role ) . '">';
+			$html .= esc_html( $label );
+			$html .= '</option>';
+		}
+
+		$html .= '</select>';
+		$html .= '<input type="hidden" name="page" />';
+		$html .= '<input type="hidden" name="order" />';
+		$html .= '<input type="hidden" name="order_by" />';
+		$html .= '<input type="hidden" name="action" value="ct_user_list_get_user_data" />';
+		$html .= '<input type="hidden" name="_wpnonce" value="' . esc_attr( wp_create_nonce( CT_USER_LIST_GET_USER_DATA_NONCE ) ) . '" />';
+		$html .= '</form>';
+		$html .= '<div class="ct-user-list">';
+		$html .= '</div>';
+		$html .= '</div>';
+
+		$fields = ct_user_list_get_fields();
+
+		foreach ( $fields as $id => $field ) {
+			$headers[ $id ] = array(
+				'label'    => $field['label'],
+				'sortable' => ! empty( $field['order_field'] ),
+			);
+		}
+
+		wp_enqueue_style( 'ct-user-list' );
+
+		wp_enqueue_script( 'ct-user-list' );
+
+		wp_localize_script(
+			'ct-user-list',
+			'ct_user_list_options',
+			array(
+				'data'        => ct_user_list_get_user_data(),
+				'headers'     => $headers,
+				'page_length' => CT_USER_LIST_PAGE_LENGTH,
+				'labels'      => array(
+					'noUsersFound' => __( 'No users found', 'ct-user-list' ),
+					'previousPage' => __( 'Previous page', 'ct-user-list' ),
+					'nextPage'     => __( 'Next page', 'ct-user-list' ),
+				),
+			)
+		);
+
+		return $html;
+	}
+);
--- public/js/ct-user-list.js
+++ public/js/ct-user-list.js
@@ -5,101 +5,209 @@
  */
 
 if ( window.ct_user_list_options ) {
-	jQuery( function( $ ) {
-		var options    = ct_user_list_options,
-			data       = options.data,
-			$container = $( '#ct-user-list-container' ),
-			$list      = $container.find( '.ct-user-list' ),
-			$search    = $container.find( '.ct-user-list-search' );
-			
-		var updateTable = function() {
-			var total  = data.total,
-				labels = options.labels;
+	jQuery(
+		function( $ ) {
+			var options    = ct_user_list_options,
+				data       = options.data,
+				pageLength = options.page_length,
+				$container = $( '#ct-user-list-container' ),
+				$list      = $container.find( '.ct-user-list' ),
+				$search    = $container.find( '.ct-user-list-search' );
 
-			$list.empty();
+				var setInput = function( name, value ) {
+					var form = $search.get( 0 );
 
-			if ( ! total ) {
-				$list.text( labels.notFound );
-			} else {
-				var $table  = $( '<table/>' ),
-					$row    = $( '<tr/>' );
-					headers = options.headers,
-					users   = data.users;
+					form[ name ].value = value;
+				};
 
-				for ( var i in headers ) {
-					var header = headers[ i ],
-						$cell   = $( '<th/>' );
+				var getInput = function( name ) {
+					var form = $search.get( 0 );
 
-					if ( header.sortable ) {
-						$cell
-							.append( $( '<a/>' )
-								.attr( 'href', '#' )
-								.data( 'field', i )
-								.text( header.label )
-								.click( function( e ) {
-									e.preventDefault();
-									// @todo
+					return form[ name ].value;
+				};
 
-									$search.submit();
-								} )
-							);
+				var setOrderBy = function( orderBy ) {
+					var oldOrderBy = getInput( 'order_by' ),
+						oldOrder   = getInput( 'order' ),
+						order;
+
+					if ( orderBy === oldOrderBy ) {
+						order = oldOrder === 'ASC' ? 'DESC' : 'ASC';
 					} else {
-						$cell.text( header.label );
+						order = 'ASC';
 					}
 
-					$row.append( $cell );
-				}
+					setInput( 'order_by', orderBy );
+					setInput( 'order', order );
+				};
 
-				$table.append( $row );
+				var updateTable = function() {
+					var total  = data.total,
+						labels = options.labels;
 
-				for ( var i in users ) {
-					var user = users[ i ];
+					$list.empty();
 
-					$row = $( '<tr/>' );
+					if ( ! total ) {
+						$list.text( labels.noUsersFound );
+					} else {
+						var $table = $( '<table/>' ),
+						$row       = $( '<tr/>' );
+						headers    = options.headers,
+						users      = data.users;
 
-					for ( var j in headers ) {
-						var header = headers[ j ],
-							$cell  = $( '<td/>' );
+						for ( var field in headers ) {
+							var header = headers[ field ],
+								$cell  = $( '<th/>' );
 
-						$cell.text( user[ j ] );
+							if ( header.sortable ) {
+								var orderBy = getInput( 'order_by' ),
+									$anchor = $( '<a/>' )
+									.attr( 'href', '#' )
+									.data( 'field', field )
+									.text( header.label )
+									.click(
+										function( e ) {
+											e.preventDefault();
 
-						$row.append( $cell );
+											setOrderBy( $( this ).data( 'field' ) );
+
+											$search.submit();
+										}
+									);
+
+								if ( orderBy === field ) {
+									var order = getInput( 'order' );
+
+									$anchor.addClass( 'ct-user-list-ordered-' + ( order === 'ASC' ? 'ascending' : 'descending' ) );
+								}
+
+								$cell.append( $anchor );
+							} else {
+								$cell.text( header.label );
+							}
+
+							$row.append( $cell );
+						}
+
+						$table.append( $row );
+
+						for ( var i in users ) {
+							var user = users[ i ];
+
+							$row = $( '<tr/>' );
+
+							for ( var j in headers ) {
+								var header = headers[ j ],
+									$cell  = $( '<td/>' );
+
+								$cell.text( user[ j ] );
+
+								$row.append( $cell );
+							}
+
+							$table.append( $row );
+						}
+
+						$list.append( $table );
+
+						if ( total > users.length ) {
+							var $pagination = $( '<div/>' ),
+								page        = parseInt( getInput( 'page' ) ) || 1,
+								maxPage     = Math.ceil( total / pageLength ),
+								links       = [];
+
+							if ( page > 1 ) {
+								links.push( [ page - 1, labels.previousPage ] );
+							}
+
+							for ( var i = Math.max( 1, page - 5 ); i <= Math.min( maxPage, page + 5 ); i++ ) {
+								links.push( i );
+							}
+
+							if ( page < maxPage ) {
+								links.push( [ page + 1, labels.nextPage ] );
+							}
+
+							for ( var i in links ) {
+								var link = links[ i ],
+									number,
+									label,
+									$page;
+
+								if ( typeof link === 'number' ) {
+									number = label = link;
+								} else {
+									number = link[0];
+									label  = link[1];
+								}
+
+								if ( number === page ) {
+									$page = $( '<span/>' );
+								} else {
+									$page = $( '<a/>' )
+										.attr( 'href', '#' )
+										.data( 'page', number )
+										.click(
+											function( e ) {
+												e.preventDefault();
+
+												setInput( 'page', $( this ).data( 'page' ) );
+
+												$search.submit();
+											}
+										);
+								}
+
+								$page.text( ' ' + label + ' ' );
+
+								$pagination.append( $page );
+							}
+
+							$list.append( $pagination );
+						}
 					}
+				};
 
-					$table.append( $row );
-				}
+				$( $search.get( 0 ).role ).change(
+					function() {
+						setInput( 'page', 1 );
 
-				$list.append( $table );
+						$search.submit();
+					}
+				);
 
-				if ( total > users.length ) {
-					// @todo
-					var $pagination = $( '<div/>' );
+				$search.submit(
+					function( e ) {
+						e.preventDefault();
 
-					$list.append( $pagination );
-				}
-			}
-		};
+						var formData = $search.serialize();
 
-		$( $search.get(0).role ).change( function() {
-			$search.submit();
-		} );
+						$container
+						.addClass( 'ct-user-list-loading' )
+						.find( ':input' )
+						.attr( 'disabled', 'disabled' );
 
-		$search.submit( function( e ) {
-			e.preventDefault();
+						$.post(
+							{
+								url      : $search.attr( 'action' ),
+								data     : formData,
+								success  : function( response ) {
+									data = response;
 
-			$search.find( ':input' ).attr( 'disabled', 'disabled' );
-			$list.find( 'a' ).css( 'pointer-events', 'none' );
-
-			$.post( $search.attr( 'action' ), $search.serialize(), function( response ) {
-				$search.find( ':input' ).removeAttr( 'disabled' );
-				$list.find( 'a' ).css( 'pointer-events', 'all' )
-
-				data = response;
+									updateTable();
+								},
+								complete : function( response ) {
+									$container
+									.removeClass( 'ct-user-list-loading' )
+									.find( ':input' )
+									.removeAttr( 'disabled' );
+								}
+							}
+						);
+					}
+				);
 
 				updateTable();
-			} );
-		} );
-
-		updateTable();
-	} );
+		}
+	);
 }
